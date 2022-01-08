f249b2cd49c739bdbe7b66fe0b3c7aceead4555c
Kirk Haines <wyhaines@gmail.com>
2022-01-07 18:07:24 -0700

    Adding the reset stream frame type.

--- spec/priority_frame_spec.cr
+++ spec/priority_frame_spec.cr
@@ -13,5 +13,19 @@ describe HTTP2::Frame::Priority do
     frame.type_code.should eq 0x02_u8
     frame.exclusive?.should be_true
     frame.stream_id.should eq 0x12345678_u32
+    frame.error?.should be_falsey
+  end
+
+  it "can report error states" do
+    buffer = IO::Memory.new
+    e_and_dependency = Bytes.new(4, 0)
+    IO::ByteFormat::BigEndian.encode(0x1234abcd, e_and_dependency)
+    # Set the E bit.
+    e_and_dependency[0] = e_and_dependency[0] | 0b10000000
+    buffer.write e_and_dependency
+    buffer.write_byte 0x64_u8
+    buffer.write_byte 0x00_u8 # extra byte
+    frame = HTTP2::Frame::Priority.new(0x00_u8, 0x12345678_u32, buffer.to_slice)
+    frame.error?.should be_truthy
   end
 end
--- src/frame/reset_stream.cr
+++ src/frame/reset_stream.cr
@@ -1,5 +1,17 @@
 module HTTP2
   struct Frame::ResetStream
     TypeCode = 0x03_u8
+
+    def error_code
+      IO::ByteFormat::BigEndian(UInt32, payload)
+    end
+
+    def error?
+      if stream_id == 0x00
+        HTTP2::ProtocolError.new("PRIORITY frame must have non-zero stream ID")
+      elsif payload.size != 4
+        HTTP2::ProtocolError.new("PRIORITY frame payload must be 5 bytes")
+      end
+    end
   end
 end
